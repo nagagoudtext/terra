d92f9c208db4c061ee083bb0f288f04a2b742f5b
chinni goud <137379865+nagagoudtext@users.noreply.github.com>
2023-09-11 18:52:11 +0530

    Add files via upload

---
+++ variable3.tf
@@ -0,0 +1,13 @@
+variable "ami" {
+type = string
+}
+
+variable "instance_type" {
+type = string 
+}
+
+variable "instance_name" {
+description = "this name tag for ec2 instance."
+type = string
+}
+
