effb02b26817a6d9611ef9d4a0dad5d0514f00e5
chinni goud <137379865+nagagoudtext@users.noreply.github.com>
2023-09-11 18:51:22 +0530

    Add files via upload

---
+++ chinni2.tf
@@ -0,0 +1,8 @@
+resource "aws_iam_user" "four" {
+name = var.user_name
+}
+
+resource "aws_iam_group" five" {
+name = var.group_name
+}
+
---
+++ variable2.tf
@@ -0,0 +1,10 @@
+variable "user_name" {
+description = "this name tag for user name."
+type = string
+}
+
+variable "group_name" {
+description = "this name tag for group_name."
+type = string
+}
+
